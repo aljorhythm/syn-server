0308cb31da0d2f32dbf01e607b5737df4c177095
Joel Lim <joel.lim@Joels-MacBook-Pro.local>
2022-04-25 18:39:12 +0800

    add clip router

--- app.spec.ts
+++ app.spec.ts
@@ -7,7 +7,19 @@ describe("new app", function () {
   beforeEach(function () {
     anApp = newApp();
   });
+
   it("responds to /", function testHome(done) {
     request(anApp).get("/").expect(200, "helloworld", done);
+  });
+});
+
+describe("user can add clip", function () {
+  let anApp: express.Express;
+  beforeEach(function () {
+    anApp = newApp();
+  });
+
+  it("POST /clip", function testHome(done) {
+    request(anApp).post("/clip").expect(200, done);
   });
 });
--- app.ts
+++ app.ts
@@ -1,11 +1,24 @@
 import express from "express";
 
+function initClipRouter() {
+  const ClipRouter = express.Router();
+
+  ClipRouter.post("/", (_, res) => {
+    res.status(200);
+    res.send();
+  });
+
+  return ClipRouter;
+}
+
 export default function newApp() {
   const app = express();
 
   app.get("/", (_, res) => {
     res.send("helloworld");
   });
+
+  app.use("/clip", initClipRouter());
 
   return app;
 }
